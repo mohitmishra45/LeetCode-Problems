fdfc44b0cd9b3e93dea4f37618bb186a5ffa1db7
Mohit Kumar Mishra <149972919+mohitmishra45@users.noreply.github.com>
2026-02-04 13:51:34 +0530

    Time: 4 ms (11.83%), Space: 23.6 MB (98.04%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0; //Tracking of non-zero
+        for(int k=0;k<nums.size();k++)
+        {
+            if(nums[k]!=0)
+            {
+                swap(nums[i],nums[k]);
+                i++;
+            }
+        }
+    }
+};
