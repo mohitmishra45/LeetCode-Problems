31685a43dc0a8942e701bab203b72818deba6b97
Mohit Kumar Mishra <149972919+mohitmishra45@users.noreply.github.com>
2026-02-23 14:36:54 +0530

    Time: 0 ms (100%), Space: 20.8 MB (98.9%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    void dfs(TreeNode* root, int target,
-             vector<int>& path,
-             vector<vector<int>>& ans)
-    {
-        if(!root) return;
+    void helper(TreeNode* root, int target, vector<int>& path,
+             vector<vector<int>>& ans) {
+        if (root==NULL)
+            return;
 
         path.push_back(root->val);
 
-        if(!root->left && !root->right && target == root->val)
-        {
+        if (root->left==NULL&& root->right==NULL && target == root->val) {
             ans.push_back(path);
+        } else {
+            helper(root->left, target - root->val, path, ans);
+            helper(root->right, target - root->val, path, ans);
         }
-        else
-        {
-            dfs(root->left, target - root->val, path, ans);
-            dfs(root->right, target - root->val, path, ans);
-        }
-
-        path.pop_back();  // backtrack
+        path.pop_back();
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> ans;
         vector<int> path;
-        dfs(root, targetSum, path, ans);
+        helper(root, targetSum, path, ans);
         return ans;
     }
 };
