d22f7a345743de400133cb13cced4a9b947fb3ef
Mohit Kumar Mishra <149972919+mohitmishra45@users.noreply.github.com>
2026-03-14 15:08:08 +0530

    Time: 44 ms (76.29%), Space: 33.1 MB (14.77%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        vector<vector<int>> result;
-        int n = nums.size();
-        
-        if(n < 3) return result;
-        
-        sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < n-2; i++)
+    vector<vector<int>>result;
+    void twoSum(vector<int>&nums,int target,int i,int j)
+    {
+        while(i<j)
         {
-            // Skip duplicates
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            
-            // Optimization: if current number > 0 break
-            if(nums[i] > 0) break;
-            
-            int left = i + 1;
-            int right = n - 1;
-            
-            while(left < right)
+            int sum=nums[i]+nums[j];
+            if(sum>target) j--;
+            else if(sum<target) i++;
+            else
             {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum == 0)
-                {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
-                    
-                    left++;
-                    right--;
-                }
-                else if(sum < 0)
-                    left++;
-                else
-                    right--;
+                while(i<j && nums[i]==nums[i+1]) i++;
+                while(i<j && nums[j]==nums[j-1]) j--;
+                result.push_back({-target,nums[i],nums[j]});
+                i++;
+                j--;
             }
         }
-        
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.size()<3) return {};
+        //Sorting
+        sort(nums.begin(),nums.end());
+        result.clear();
+        //Fixing One Value
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+        int n1=nums[i];
+        int target=-n1;
+        twoSum(nums,target,i+1,nums.size()-1);
+        }
         return result;
     }
 };
